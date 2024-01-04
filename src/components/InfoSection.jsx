1932aef4503d02ee340e8ae9c0ea5044ceff56c5
yari-dewalt <yari.dewalt@gmail.com>
2024-01-04 14:52:48 -0800

    Add check to only render the header for if there are items in the array

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -1,7 +1,7 @@
 function InfoSection( { array, InfoComponent, title }) {
   return (
     <div className="info-section">
-      <h3 className="section-title">{title}</h3>
+      {array.length > 0 && <h3 className="section-title">{title}</h3>}
       {array.map((data) => 
         <InfoComponent data={data} key={data.id}/>
       )}
