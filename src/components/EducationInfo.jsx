103c66d0f0611cb598c91aa46a7fcdc06d4b332f
yari-dewalt <yari.dewalt@gmail.com>
2024-01-03 19:09:19 -0800

    Create EducationInfo component to display data from EducationForm components

---
+++ src/components/EducationInfo.jsx
@@ -0,0 +1,21 @@
+import "../styles/EducationInfo.css";
+
+function EducationInfo({ data }) {
+  const { schoolName, degree, location, startDate, endDate } = data;
+  return (
+    <>
+      <div className="education-info">
+        <div className="education-info-group">
+          <p className="education-date">{`${startDate} - ${endDate}`}</p>
+          <p className="education-location">{location}</p>
+        </div>
+        <div className="education-info-group">
+          <p className="education-schoolName">{schoolName}</p>
+          <p className="education-degree">{degree}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default EducationInfo;
