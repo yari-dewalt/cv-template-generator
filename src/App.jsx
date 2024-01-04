55268485e06deabe937df0e9a9b37e51cf14e613
yari-dewalt <yari.dewalt@gmail.com>
2024-01-03 19:04:55 -0800

    Rewrite functions to fit new data and multiple forms and include new components for scalability

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,66 @@
 import { useState } from 'react'
+import data from "./data.jsx";
 import './App.css'
-import InputGroup from "./components/InputGroup.jsx";
 import PersonalDetails from "./components/PersonalDetails.jsx";
 import PersonalInfoSection from "./components/PersonalInfoSection.jsx";
+import ExpandSection from "./components/ExpandSection.jsx";
+import EducationForm from "./components/EducationForm.jsx";
+import EducationInfo from "./components/EducationInfo.jsx";
 
 function App() {
-  const [personalDetails, setPersonalDetails] = useState({
-    "Full name": "Jane Doe",
-    "Email": "jane.doe@gmail.com",
-    "Phone number": "(123) 456-7890",
-    "Location": "Los Angeles, CA",
-  });
+  const [personalDetails, setPersonalDetails] = useState(data.personalInfo);
 
   const handlePersonalDetailsChange = (e) => {
     const { key } = e.target.dataset;
-    setPersonalDetails((prevValues) => ({
-      ...prevValues,
-      [key]: e.target.value,
-    }));
+    setPersonalDetails({
+      ...personalDetails,
+      [key]: e.target.value
+    });
   };
+
+  const [educationDetails, setEducationDetails] = useState(data.sections.educations[0]);
+
+  const [sections, setSections] = useState(data.sections);
+
+  function handleSectionChange(e) {
+    const { key } = e.target.dataset;
+    const inputValue = e.target.value;
+    const form = e.target.closest(".section-form");
+    const { id } = form;
+    const { arrayName } = form.dataset;
+    const section = sections[arrayName];
+    setSections({
+      ...sections,
+      [arrayName]: section.map((obj) => {
+        if (obj.id === id) obj[key] = inputValue;
+        return obj;
+      }),
+    });
+  }
+
+  const [sectionOpen, setSectionOpen] = useState(null);
+  const setOpen = (sectionName) => setSectionOpen(sectionName);
+
 
   return (
     <>
       <div id="edit">
-        <PersonalDetails onChange={handlePersonalDetailsChange} fullName={personalDetails["Full name"]} email={personalDetails["Email"]} phoneNumber={personalDetails["Phone number"]} location={personalDetails["Location"]}/>
+        <PersonalDetails
+          onChange={handlePersonalDetailsChange}
+          fullName={personalDetails.fullName}
+          email={personalDetails.email}
+          phoneNumber={personalDetails.phoneNumber}
+          location={personalDetails.location}
+        />
+        <ExpandSection isOpen={sectionOpen === "Education"} setOpen={setOpen} sectionName="Education" iconSource="../public/education.svg"/>
+        <EducationForm data={educationDetails} onChange={handleSectionChange}/>
       </div>
       <div id="preview">
-        <PersonalInfoSection fullName={personalDetails["Full name"]} email={personalDetails["Email"]} phoneNumber={personalDetails["Phone number"]} location={personalDetails["Location"]}/>
+        <PersonalInfoSection fullName={personalDetails.fullName} email={personalDetails.email} phoneNumber={personalDetails.phoneNumber} location={personalDetails.location}/>
+        <div>
+          <h3>Education</h3>
+          <EducationInfo data={educationDetails}/>
+        </div>
       </div>
     </>
   )
