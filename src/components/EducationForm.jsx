1e8faf05d5fd4890a4deba7320c93525c162880c
yari-dewalt <yari.dewalt@gmail.com>
2024-01-03 21:51:34 -0800

    Add Buttons component to part of EducationForm and pass their handlers as props

--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -1,60 +1,61 @@
 import "../styles/EducationForm.css";
 import InputGroup from "./InputGroup.jsx";
+import Buttons from "./Buttons.jsx";
 
-function EducationForm({ data, onChange }) {
+function EducationForm({ data, onChange, save, remove }) {
   const { degree, schoolName, location, startDate, endDate, id } = data;
 
   return (
-    <>
-      <form
-        className="education-form section-form"
-        id={id}
-        data-array-name="educations"
-        onSubmit={(e) => e.preventDefault()}
-      >
-        <InputGroup
-          id="school-name"
-          placeholder="Enter school / university"
-          labelText="School"
-          value={schoolName}
-          onChange={onChange}
-          data-key="schoolName"
-        /> 
-        <InputGroup
-          id="degree"
-          placeholder="Enter Degree / Field of Study"
-          labelText="Degree"
-          value={degree}
-          onChange={onChange}
-          data-key="degree"
-        /> 
-        <InputGroup
-          id="date"
-          placeholder="Enter Start Date"
-          labelText="Start Date"
-          value={startDate}
-          onChange={onChange}
-          data-key="startDate"
-        /> 
-        <InputGroup
-          id="date"
-          placeholder="Enter End Date"
-          labelText="End Date"
-          value={endDate}
-          onChange={onChange}
-          data-key="endDate"
-        /> 
-        <InputGroup
-          id="location"
-          placeholder="Enter Location"
-          labelText="Location"
-          value={location}
-          onChange={onChange}
-          data-key="location"
-        /> 
-      </form>
-    </>
-  )
+    <form
+      className="education-form section-form"
+      id={id}
+      data-array-name="educations"
+      onSubmit={(e) => e.preventDefault()}
+    >
+      <InputGroup
+        id="school-name"
+        placeholder="Enter school / university"
+        labelText="School"
+        value={schoolName}
+        onChange={onChange}
+        data-key="schoolName"
+      /> 
+      <InputGroup
+        id="degree"
+        placeholder="Enter Degree / Field of Study"
+        labelText="Degree"
+        value={degree}
+        onChange={onChange}
+        data-key="degree"
+      /> 
+      <InputGroup
+        id="date"
+        placeholder="Enter Start Date"
+        labelText="Start Date"
+        value={startDate}
+        onChange={onChange}
+        data-key="startDate"
+      /> 
+      <InputGroup
+        id="date"
+        placeholder="Enter End Date"
+        labelText="End Date"
+        value={endDate}
+        onChange={onChange}
+        data-key="endDate"
+      /> 
+      <InputGroup
+        id="location"
+        placeholder="Enter Location"
+        labelText="Location"
+        value={location}
+        onChange={onChange}
+        data-key="location"
+      /> 
+
+      <Buttons save={save} remove={remove}/>
+    </form>
+  );
 }
 
 export default EducationForm;
