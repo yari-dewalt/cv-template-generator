52a8b9914f5be0a481d28a5b7af280fb31c669db
yari-dewalt <yari.dewalt@gmail.com>
2024-01-04 15:24:38 -0800

    Create EditEducationSection component combining ExpandSection button component and DisplayForms component

---
+++ src/components/EditEducationSection.jsx
@@ -0,0 +1,42 @@
+import DisplayForms from "./DisplayForms.jsx";
+import ExpandSection from "./ExpandSection.jsx";
+import CreateForm from "./CreateForm.jsx";
+import EducationForm from "./EducationForm.jsx";
+import "../styles/Section.css";
+
+function EditEducationSection({
+  educations,
+  isOpen,
+  setOpen,
+  onChange,
+  createForm,
+  toggleCollapsed,
+  onRemove
+}) {
+  return (
+    <div className="edit-education-section section">
+      <ExpandSection
+        isOpen={isOpen}
+        setOpen={setOpen}
+        sectionName="Education"
+        iconSource="../../public/education.svg"
+       />
+
+      <div className={`section-content ${isOpen ? "open" : ""}`}>
+        <DisplayForms
+          forms={educations}
+          FormComponent={EducationForm}
+          onChange={onChange}
+          onRemove={onRemove}
+          toggleCollapsed={toggleCollapsed}
+          titleKey="schoolName"
+          arrayName="educations"
+        />
+
+        <CreateForm onClick={createForm} buttonText="+ Education"/>
+      </div>
+    </div>
+  );
+}
+
+export default EditEducationSection;
