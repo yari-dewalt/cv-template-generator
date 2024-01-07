292571ea2e29f59861e64fa8011f0c4cb4bc9c18
yari-dewalt <yari.dewalt@gmail.com>
2024-01-06 19:51:09 -0800

    Change icon id to generic className and fix file path

--- src/components/ExpandSection.jsx
+++ src/components/ExpandSection.jsx
@@ -4,9 +4,9 @@ function ExpandSection({ isOpen, setOpen, sectionName, iconSource }) {
   return (
     <>
       <button className="expand-section" onClick={() => setOpen(isOpen ? "" : sectionName)}>
-        <img id="education-icon" src={iconSource}></img>
+        <img className="section-icon" src={iconSource}></img>
         <h2>{sectionName}</h2>
-        <img className={`expand-icon ${isOpen ? "open" : ""}`} src="../../public/expand.svg"></img>
+        <img className={`expand-icon ${isOpen ? "open" : ""}`} src="expand.svg"></img>
       </button>
     </>
   )
